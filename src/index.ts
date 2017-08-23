e8a753cab9c4b412c8a01f7492d32bc98f8b31e1
Steffan Sluis <steffansluis@gmail.com>
2017-08-23 02:37:38 +0200

    Add quad and helpers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import * as Config from './config';
 export { Config };
 
+export * from './quad';
 export * from './doc';
+
+import * as Helpers from './helpers';
+export { Helpers };
 
 export * from './miner';
 export * from './annotator';
