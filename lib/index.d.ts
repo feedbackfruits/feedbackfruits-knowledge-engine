31a0260ade4055543dbf81cd8aecc8a08586cb7e
Steffan Sluis <steffansluis@gmail.com>
2018-02-08 22:41:23 +0100

    Bump context and re-export it for use in engines

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,12 @@
+import * as Context from 'feedbackfruits-knowledge-context';
+export { Context };
 import * as Config from './config';
 export { Config };
-export * from './quad';
-export * from './doc';
 import * as Helpers from './helpers';
 export { Helpers };
+export * from './quad';
+export * from './doc';
+export * from './engine';
 export * from './miner';
 export * from './annotator';
 export * from './broker';
